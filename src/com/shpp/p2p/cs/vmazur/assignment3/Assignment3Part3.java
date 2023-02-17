448350bcebd37838417124d156f884200b6ef7ce
Mazur <Slavaslidchum@gmail.com>
2023-02-17 13:27:03 +0200

    Task 3 level 3

---
+++ src/com/shpp/p2p/cs/vmazur/assignment3/Assignment3Part3.java
@@ -0,0 +1,42 @@
+package com.shpp.p2p.cs.vmazur.assignment3;
+
+import com.shpp.cs.a.console.TextProgram;
+
+/**
+ * Class raises a number in squarer.
+ */
+public class Assignment3Part3 extends TextProgram {
+    public void run() {
+        System.out.println(raiseToPower(0.5, -2));
+
+    }
+
+    /**
+     * Method raise number in dependency of positive or negative exponent.
+     * @param base number with will rise
+     * @param exponent number of times
+     * @return result
+     */
+    private double raiseToPower(double base, int exponent) {
+        double result = 1;
+
+        if (exponent > 0) {
+            for (int i = 1; i <= exponent; i++) {
+                result = result * base;
+
+            }
+        }
+
+        if (exponent < 0) {
+            exponent = exponent * -1;
+
+            for (int i = 1; i <= exponent; i++) {
+                result = result * base;
+            }
+            result = 1 / result;
+        }
+        return result;
+    }
+}
+
+
